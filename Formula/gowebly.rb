58bb76cee1aa673c89d923c32a3339d3bfe004c8
Vic Shóstak <koddr.me@gmail.com>
2023-10-04 15:32:37 +0300

    Brew formula update for gowebly version v1.2.0

--- Formula/gowebly.rb
+++ Formula/gowebly.rb
@@ -6,21 +6,21 @@ class Gowebly < Formula
   desc "A next-generation CLI tool for building amazing web applications in Go using htmx & hyperscript and the most popular atomic/utility-first CSS frameworks.
 "
   homepage "https://github.com/gowebly/gowebly"
-  version "1.1.0"
+  version "1.2.0"
   license "Apache 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gowebly/gowebly/releases/download/v1.1.0/gowebly_1.1.0_darwin_arm64.tar.gz"
-      sha256 "5cb0a058a741400d683519471aa321a5688f30b9fa8b1b1760b5f79bbb5c9560"
+      url "https://github.com/gowebly/gowebly/releases/download/v1.2.0/gowebly_1.2.0_darwin_arm64.tar.gz"
+      sha256 "559df550fa857802d16b4ca92e283762ca03a94c35fe69d1f276506c492b3d4d"
 
       def install
         bin.install "gowebly"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gowebly/gowebly/releases/download/v1.1.0/gowebly_1.1.0_darwin_amd64.tar.gz"
-      sha256 "c81b5bbee26b5ccb9a2ced93a38b7a79e22d7e059d5204bd6759f92d6bbceae5"
+      url "https://github.com/gowebly/gowebly/releases/download/v1.2.0/gowebly_1.2.0_darwin_amd64.tar.gz"
+      sha256 "68cc69879a3a32b1c8d44963e60bb57233480f289e3d71d5d6162f33aff26950"
 
       def install
         bin.install "gowebly"
@@ -30,16 +30,16 @@ class Gowebly < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/gowebly/gowebly/releases/download/v1.1.0/gowebly_1.1.0_linux_arm64.tar.gz"
-      sha256 "de1e62993f57f40962701c4b64a5b3ac47e182fe6ad49e426bb02feab413b43e"
+      url "https://github.com/gowebly/gowebly/releases/download/v1.2.0/gowebly_1.2.0_linux_arm64.tar.gz"
+      sha256 "6f637e51f2f127bb7deef0554291a9b954071096e9c99a807c462acbbb3b1ffd"
 
       def install
         bin.install "gowebly"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gowebly/gowebly/releases/download/v1.1.0/gowebly_1.1.0_linux_amd64.tar.gz"
-      sha256 "b0cdb68a277a11727fdff7a0949a5ee3fd99ce0a62e94521957f3e3904fd3355"
+      url "https://github.com/gowebly/gowebly/releases/download/v1.2.0/gowebly_1.2.0_linux_amd64.tar.gz"
+      sha256 "97c91a8c12f797241528083af7e0dbf21040c0854af1aec8b944397b1d920f10"
 
       def install
         bin.install "gowebly"
